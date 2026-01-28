6fb8fe79ae5b9167300d4ed89bf4d4cf6e759845
Ignacio <ignayh2000@gmail.com>
2026-01-28 09:30:31 +0100

    Create Tonic of Downfall testsç

---
+++ __test__/tonicDownfall.test.js
@@ -0,0 +1,86 @@
+const { Cauldron } = require("../src/cauldron");
+
+
+describe(`Crear el Tonic of Downfall.`, () => {
+
+    it(`Cuando tienes menos de 2 ingredientes`, () => {
+        const ingedients = [];
+
+        const potion = Cauldron.createPotion(ingedients);
+
+        console.log(potion);
+
+        expect(potion.name).toBe('Tonic of Downfall');
+    });
+
+
+    it(`Cuando tienes más de 4 ingredientes`, () => {
+        const ingedients = [
+            {
+                "_id": "6702b44f76863c206a48cce8",
+                "name": "Giant's Tear",
+                "description": "A tear known for its ability to enhance strength and fortitude.",
+                "value": 250,
+                "effects": [
+                    "greater_restore_strength"
+                ],
+                "image": "/images/ingredients/restore/restore_19.webp",
+                "type": "ingredient"
+            },
+
+            {
+                "_id": "6702b44f76863c206a48cce1",
+                "name": "Tranquility Flower",
+                "description": "A tranquil flower that helps to alleviate insanity and calm the mind.",
+                "value": 5,
+                "effects": [
+                    "least_restore_insanity"
+                ],
+                "image": "/images/ingredients/restore/restore_12.webp",
+                "type": "ingredient"
+            },
+
+            {
+                "_id": "6702b44f76863c206a48ccd9",
+                "name": "Fortitude Root",
+                "description": "A potent root that strengthens the constitution and fortifies the body.",
+                "value": 270,
+                "effects": [
+                    "greater_restore_constitution"
+                ],
+                "image": "/images/ingredients/restore/restore_4.webp",
+                "type": "ingredient"
+            },
+
+            {
+                "_id": "6702b44f76863c206a48ccdc",
+                "name": "Titan's Blood",
+                "description": "A rare blood known for its ability to enhance strength tremendously.",
+                "value": 75,
+                "effects": [
+                    "restore_strength"
+                ],
+                "image": "/images/ingredients/restore/restore_7.webp",
+                "type": "ingredient"
+            },
+
+            {
+                "_id": "6702b44f76863c206a48ccea",
+                "name": "Breeze Blossom",
+                "description": "A light flower that gently restores dexterity and movement.",
+                "value": 6,
+                "effects": [
+                    "least_restore_dexterity"
+                ],
+                "image": "/images/ingredients/restore/restore_21.webp",
+                "type": "ingredient"
+            },
+
+        ];
+
+        const potion = Cauldron.createPotion(ingedients);
+        expect(potion.name).toBe('Tonic of Downfall');
+
+        
+    })
+});
